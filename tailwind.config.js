408b82f3c924dd67e2d61d9f3737144c773e03bf
armcruz <jaimes087.uz@gmail.com>
2021-08-16 21:53:10 -0500

    style: add card typography styles and info styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ module.exports = {
 				'body-pattern-m': 'url(../images/pattern-background-mobile.svg)',
 				'body-pattern-d': 'url(../images/pattern-background-desktop.svg)',
 			}),
+			fontFamily: {
+				body: ['"Red Hat Display"', 'sans-serif'],
+			},
+			colors: {
+				'pale-blue': 'hsl(225, 100%, 94%)',
+				'bright-blue': 'hsl(245, 75%, 52%)',
+				'very-pale-blue': 'hsl(225, 100%, 98%)',
+				'desaturated-blue': 'hsl(224, 23%, 55%)',
+				'dark-blue': 'hsl(223, 47%, 23%)',
+			},
 		},
 	},
 	variants: {
